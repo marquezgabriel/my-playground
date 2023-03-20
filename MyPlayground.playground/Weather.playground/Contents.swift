e7add6885fda0a1c4e6f4fa32f539e2b9411170c
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-03-20 10:03:07 -0400

    Create Operated and Data Types

---
+++ MyPlayground.playground/Weather.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+let day: String = "Monday"
+let dailyTemperature: Int = 75
+var temperature: Int = 70
+
+print("Today is \(day). Rise and shine!")
+print("The temperature on \(day) is \(dailyTemperature)°F")
+print("The Temperature on \(day) Morning is \(temperature)°F")
+
+temperature  =  80
+
+print("The Temperature on \(day) Evening is \(temperature)°F")
+
+let weeklyTemperature = 75
+temperature = weeklyTemperature
+
+print("The average temperature this week is \(temperature)°F.")
