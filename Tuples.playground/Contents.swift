3d89e30dc89f75e76594bba89194be691195e3ee
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-03-27 16:19:00 -0400

    Tuples

--- Tuples.playground/Contents.swift
+++ Tuples.playground/Contents.swift
@@ -1,3 +1,20 @@
-import Cocoa
 
-var greeting = "Hello, playground"
+let credentials = ("", -1111)
+
+if (credentials.0 == "") || (credentials.1 < 0) {
+    print("Invalid credentials!")
+} else {
+    print("The username is \(credentials.0) and the passcode is \(credentials.1)")
+}
+
+
+let fullCredentials = (password:"pass", passcode:1111)
+
+if (fullCredentials.password != "") && (fullCredentials.passcode > 0) {
+    print("The password is '\(fullCredentials.password)' and the passcode is \(fullCredentials.passcode)")
+} else {
+    print("Invalid credentials!")
+}
+
+let emptyArray: [Int] = []
+print(emptyArray.count)
