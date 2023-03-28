cf40da7404d828684699cb9a4c873ef5598f9f0b
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-03-28 16:35:41 -0400

    Dictionaries

---
+++ Dictionaries.playground/Contents.swift
@@ -0,0 +1,19 @@
+
+
+var weeklyTemperatures:[String:Int] = [:]
+
+
+weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95]
+
+weeklyTemperatures["Monday"]! += 20
+
+print("The temperatue on Monday is \(weeklyTemperatures["Monday"]!) F.")
+
+let numberOfDays = (weeklyTemperatures.count - 1)
+let temperature = 
+
+if let temperature = weeklyTemperature["Sunday"] == nil {
+    
+} else {
+    
+}
