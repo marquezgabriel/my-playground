08909c43754e8c5008e03eb25d2840775b0cb671
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-03-28 20:02:55 -0400

    Dictionaries exercise

--- Dictionaries.playground/Contents.swift
+++ Dictionaries.playground/Contents.swift
@@ -1,19 +1,17 @@
-
-
-var weeklyTemperatures:[String:Int] = [:]
-
-
-weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95]
-
+var weeklyTemperatures: [String: Int] = [:]
+weeklyTemperatures = [
+  "Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95,
+]
 weeklyTemperatures["Monday"]! += 20
-
-print("The temperatue on Monday is \(weeklyTemperatures["Monday"]!) F.")
-
-let numberOfDays = (weeklyTemperatures.count - 1)
-let temperature = 
-
-if let temperature = weeklyTemperature["Sunday"] == nil {
-    
+print("The temperature on Monday is \(weeklyTemperatures["Monday"]!)°F.")
+if let temperature = weeklyTemperatures["Sunday"] {
+  print("The temperature on Sunday is \(temperature)°F.")
 } else {
-    
+  weeklyTemperatures["Sunday"] = 100
+  print("The temperature on Sunday is \(weeklyTemperatures["Sunday"]!)°F.")
+}
+if weeklyTemperatures.count == 7 {
+  print("You have access to the weather forecast of the whole week.")
+  weeklyTemperatures = [:]
+  print("Reset weekly temperatures for next week!")
 }
