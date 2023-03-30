92ec8bfb27d22240a81a989eda09ea2351bb6ef6
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-03-30 14:32:51 -0400

    Exercise Funtions

---
+++ ExercisePracticeWritingSuccinctlyExpressiveFunctions.playground/Contents.swift
@@ -0,0 +1,21 @@
+
+
+var goldBars:Int = 0
+
+func incrementInventory(_ inventory: inout Int, by amount:Int = 100) -> Int {
+    inventory += amount
+    return inventory
+}
+
+var inventory = 0
+
+for _ in 1...3 {
+    incrementInventory(&goldBars)
+    print(goldBars)
+}
+
+incrementInventory(&goldBars, by: 300)
+print(goldBars)
+
+incrementInventory(&goldBars, by: 50)
+print(goldBars)
