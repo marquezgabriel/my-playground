6e88eeddffd06b82bdce606a2943459b7d474bb9
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-04-08 16:48:31 -0400

    Starting week 5

--- FinalProjectTheBankAccountSimulator.playground/Contents.swift
+++ FinalProjectTheBankAccountSimulator.playground/Contents.swift
@@ -1,4 +1,49 @@
 
+class OpenBankAccount {
+    var accountType:String = ""
+    var numberPadKey:Int = 0
+    
+    init (accountType:String){
+        self.accountType = accountType
+        
+    }
+    
+    func welcomeText() {
+        print("Welcome to your virtual bank system.")
+    }
+
+    func optionsMenu() {
+        print("What kind of account would you like to open?")
+        print("1. Debit account")
+        print("2. Credit account")
+    }
+    
+    func bankAccountType() {
+        //var numberPadKey:Int = 0
+            repeat {
+              // Read input from the user
+              let numberPadKey = Int(readLine() ?? "0") ?? 0
+              
+              // Evaluate the input using a switch statement
+              switch numberPadKey {
+                case 1:
+                  print("You entered 1")
+                case 2:
+                  print("You entered 2")
+                default:
+                  print("Invalid input")
+              }
+            } while numberPadKey < 1 || numberPadKey > 3
+        
+        print("The selected option is... \(numberPadKey)")
+    }
+    
+    
+}
+
+
+
+
 
 class BankAccount {
     var accountNumber: Int
