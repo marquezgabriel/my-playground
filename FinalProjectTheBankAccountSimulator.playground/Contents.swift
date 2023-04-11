f1c962429532043eadb86430d30da2eea788083a
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-04-11 10:20:55 -0400

    Bank system - step 3 completed

--- FinalProjectTheBankAccountSimulator.playground/Contents.swift
+++ FinalProjectTheBankAccountSimulator.playground/Contents.swift
@@ -1,42 +1,17 @@
 
 class OpenBankAccount {
+    // Step
     var accountType:String = ""
-    var numberPadKey:Int = 0
-    
-    init (accountType:String){
-        self.accountType = accountType
+    // Step 3
+    func makeBankAccount(numberPadKey:Int) {
+        print("The selected option is: \(numberPadKey)")
         
-    }
-    
-    func welcomeText() {
-        print("Welcome to your virtual bank system.")
-    }
 
-    func optionsMenu() {
-        print("What kind of account would you like to open?")
-        print("1. Debit account")
-        print("2. Credit account")
+        
+        
     }
     
-    func bankAccountType() {
-        //var numberPadKey:Int = 0
-            repeat {
-              // Read input from the user
-              let numberPadKey = Int(readLine() ?? "0") ?? 0
-              
-              // Evaluate the input using a switch statement
-              switch numberPadKey {
-                case 1:
-                  print("You entered 1")
-                case 2:
-                  print("You entered 2")
-                default:
-                  print("Invalid input")
-              }
-            } while numberPadKey < 1 || numberPadKey > 3
-        
-        print("The selected option is... \(numberPadKey)")
-    }
+    
     
     
 }
@@ -45,26 +20,3 @@ class OpenBankAccount {
 
 
 
-class BankAccount {
-    var accountNumber: Int
-    var accountType: String
-    var balance: Double
-    
-    init(accountNumber: Int, accountType: String, balance: Double) {
-        self.accountNumber = accountNumber
-        self.accountType = accountType
-        self.balance = balance
-    }
-    
-    func deposit(amount: Double) {
-      balance += amount
-    }
-    
-    func withdraw(amount: Double) {
-        if balance >= amount {
-            balance -= amount
-        } else {
-            print("Insufficient balance")
-      }
-    }
-}
