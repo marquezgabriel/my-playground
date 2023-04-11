36cf0894dd8009721654abe0c34964e62949174a
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-04-11 10:56:52 -0400

    Bank system - step 4 completed

--- FinalProjectTheBankAccountSimulator.playground/Contents.swift
+++ FinalProjectTheBankAccountSimulator.playground/Contents.swift
@@ -1,20 +1,29 @@
 
 class OpenBankAccount {
-    // Step
+    
     var accountType:String = ""
-    // Step 3
+    
+    func welcomeCustomer() {
+            print("Welcome to your virtual bank system.")
+            print("What kind of account would you like to open?")
+            print("1. Debit account")
+            print("2. Credit account")
+        }
+    
     func makeBankAccount(numberPadKey:Int) {
         print("The selected option is: \(numberPadKey)")
-        
-
-        
-        
+        repeat {
+            switch numberPadKey {
+                case 1: accountType = "debit"
+                case 2: accountType = "credit"
+            default: print("Invalid input: \(numberPadKey)"); break
+            }
+        } while accountType != ""
+        print("You have opened a \(accountType) account.")
     }
-    
-    
-    
-    
 }
+
+
 
 
 
