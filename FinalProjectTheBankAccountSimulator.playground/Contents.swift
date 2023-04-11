983b964f32f0598ab5321906347ff3e1eb6d93c0
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-04-11 17:21:54 -0400

    Part 1 completed

--- FinalProjectTheBankAccountSimulator.playground/Contents.swift
+++ FinalProjectTheBankAccountSimulator.playground/Contents.swift
@@ -1,39 +1,48 @@
-// Step 1
-class OpenBankAccount {
-    var accountType:String = "" // Step 2
-    func greetings() {
+// Part 1 - Step 1
+class VirtualBankSystem {
+    var accountType:String = "" // Part 1 - Step 2
+    func welcomeCustomer() {
         print("Welcome to your virtual bank system.")
-        }
-    func onboardCustomer() {
+    }
+    func onboardCustomerAccountOpening() {
         print("What kind of account would you like to open?")
         print("1. Debit account")
         print("2. Credit account")
     }
-    // Step 3
-    func accountType(numberPadKey:Int) {
+    // Part 1 - Step 3
+    func makeAccount(numberPadKey:Int) {
         print("The selected option is: \(numberPadKey)")
-        repeat {
-            switch numberPadKey {
-                case 1: accountType = "debit"
-                case 2: accountType = "credit"
-            default: print("Invalid input: \(numberPadKey)"); break
-            }
-        } while accountType != ""
+        
+        switch numberPadKey {
+        case 1:
+            accountType = "debit"
+        case 2:
+            accountType = "credit"
+        default:
+            print("Invalid input: \(numberPadKey)")
+            return
+        }
+        
         print("You have opened a \(accountType) account.")
     }
     
 }
 
-let myBankAccount = OpenBankAccount() // Step 4
-myBankAccount.greetings()
-myBankAccount.onboardCustomer()
+let virtualBankSystem = VirtualBankSystem() // Part 1 - Step 4
+virtualBankSystem.welcomeCustomer()
 
-
+// Part 1 - Step 5
 repeat {
-    firstDice = Int.random(in: 1...5)
-    
-    
-} while accountType != ""
+    virtualBankSystem.onboardCustomerAccountOpening()
+    let numberPadKey = Int.random(in: 1...3)
+    virtualBankSystem.makeAccount(numberPadKey: numberPadKey)
+} while virtualBankSystem.accountType == ""
+
+
+
+
+
+
 
 
 
