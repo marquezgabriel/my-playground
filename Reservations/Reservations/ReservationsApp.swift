db34c9660e3a6b2e0b214ae1e19be646c42c95a9
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-05-23 09:57:47 -0400

    Create Reservation Project

---
+++ Reservations/Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by Gabriel Marquez on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
