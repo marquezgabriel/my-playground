f9d42f7de5637b53392fdf3b5b2a151110d9abb7
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-05-26 12:42:59 -0400

    Week 1.5

--- Little Lemon Demo/Little Lemon Demo.swiftpm/ContentView.swift
+++ Little Lemon Demo/Little Lemon Demo.swiftpm/ContentView.swift
@@ -1,9 +1,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputValue:String = ""
     var body: some View {
         VStack {
-            Text("Little Lemon")
+            Text("Little Lemon Restaurant")
+                    .font(.title)
+                    .foregroundColor(.gray)
+                    .background(Color.black)
+                    .padding()
+                    .background(Color.gray)
+            Label("Lightning", systemImage: "bolt.fill")
+            TextField("Type Your Name", text:$inputValue, onEditingChanged: { status in print (status)})
+                .onChange(of: inputValue, perform: { newValue in print(newValue)})
+                .onSubmit {
+                    print("submitted")
+                }
+                .padding()
         }
     }
 }
