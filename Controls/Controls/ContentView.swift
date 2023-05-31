f8bf06578df725950cea56f3047e440cdbc72a65
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-05-31 17:51:45 -0400

    Crontrols - Testing - SwiftUI

--- Controls/Controls/ContentView.swift
+++ Controls/Controls/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image("littleLemonLogo")
+            Image(systemName: "square.and.arrow.up")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
             ReservationCalendar()
         }
     }
