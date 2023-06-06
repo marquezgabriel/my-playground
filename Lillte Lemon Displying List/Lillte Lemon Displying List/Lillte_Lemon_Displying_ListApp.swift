75388d1b061ab5b61f48c4e06c22d7d832269171
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-06-06 15:21:53 -0400

    List Excercise - SwiftUI

---
+++ Lillte Lemon Displying List/Lillte Lemon Displying List/Lillte_Lemon_Displying_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lillte_Lemon_Displying_ListApp.swift
+//  Lillte Lemon Displying List
+//
+//  Created by Gabriel Marquez on 2023-06-06.
+//
+
+import SwiftUI
+
+@main
+struct Lillte_Lemon_Displying_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
