d2a0612ff9450b0f19c6bfb97b7db1b4dbff722c
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-06-08 15:58:52 -0400

    Little Lemon Logo added - SwiftUI

--- Little Lemon Navigation/Little Lemon Navigation/ContentView.swift
+++ Little Lemon Navigation/Little Lemon Navigation/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView()
+        Image("LittleLemonLogo")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
     }
 }
 
