858bb74e2d4ab2d6db697f51bbbb00b9b5b21a95
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-06-09 10:41:38 -0400

    Exercise: Gestures and callbacks solved - SwiftUI

---
+++ Little Lemon Navigation/Little Lemon Navigation/Gestures/GestureRecognizer.swift
@@ -0,0 +1,36 @@
+//
+//  GestureRecognizer.swift
+//  Little Lemon Navigation
+//
+//  Created by Gabriel Marquez on 2023-06-09.
+//
+
+import SwiftUI
+
+struct GestureRecognizer: View {
+    
+    @State var flag:Bool = false
+    
+    var body: some View {
+        Text("Tap Me!")
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .padding(15)
+            .background(flag ? Color.green : Color.yellow)
+            .cornerRadius(10)
+            .onTapGesture {
+                print("Text Tapped!")
+                flag = true
+            }
+            .onLongPressGesture {
+                print("Text Long Pressed!")
+                flag = false
+            }
+    }
+}
+
+struct GestureRecognizer_Previews: PreviewProvider {
+    static var previews: some View {
+        GestureRecognizer()
+    }
+}
