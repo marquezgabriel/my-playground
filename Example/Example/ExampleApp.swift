e33bff36cffd7dfa866d338116330711721a1840
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-06-15 16:02:09 -0400

    Example

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Gabriel Marquez on 2023-06-15.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
