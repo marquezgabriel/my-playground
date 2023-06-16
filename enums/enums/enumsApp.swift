32bedc36e951c74140d1d924ac4c640a96051676
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-06-16 14:30:11 -0400

    Example

---
+++ enums/enums/enumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  enumsApp.swift
+//  enums
+//
+//  Created by Gabriel Marquez on 2023-06-16.
+//
+
+import SwiftUI
+
+@main
+struct enumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
