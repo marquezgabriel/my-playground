3e3205fd43a16e4d6fb072f8b8479ecb02048dbe
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-06-27 14:35:50 -0400

    Implement Protocols with Classes

--- ImplementProtocolsWithClasses.playground/Contents.swift
+++ ImplementProtocolsWithClasses.playground/Contents.swift
@@ -31,3 +31,51 @@ let SuperSpaghetti = MainDish(name: "Super Spaghetti", preparationMinutes: 35)
 
 SuperSpaghetti.prepare()
 SuperSpaghetti.plate(artisticLevel: 10)
+
+// Another example
+
+protocol Employee {
+    var name: String { get }
+    var daysWorking: Int { get set }
+    
+}
+
+struct Chef: Employee {
+    let name: String
+    var daysWorking: Int
+}
+
+// Another example
+
+protocol ProtocolIdentifier1 {
+    func methodIdentifier(parameter: Int) -> Int
+    var propertyIdentifier: Int { get }
+}
+
+// Making a property identifier optional
+
+@objc protocol ProtocolIdentifier {
+    func methodIdentifier(parameter: Int) -> Int
+    @objc optional var propertyIdentifier: Int { get }
+}
+
+// Another exmaple
+
+protocol GoodChef {
+    func cookGoodFood()
+}
+protocol GreatChef {
+    func cookGreatFood()
+}
+protocol ExcellentChef: GoodChef, GreatChef {
+    func cookExcellentFood()
+}
+
+class Person: ExcellentChef {
+    func cookExcellentFood() {
+    }
+    func cookGoodFood() {
+    }
+    func cookGreatFood() {
+    }
+}
