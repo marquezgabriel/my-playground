7c727d434a4d6bd47c945dd63e8b4de2bce8ad91
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-06-27 09:13:26 -0400

    Protocols

---
+++ protocols.playground/Contents.swift
@@ -0,0 +1,11 @@
+
+protocol Employee {
+    var  name: String { get }
+    var daysWorking: Int { get set }
+    func executePrimaryDuty()
+    func danceToHappyBirthdaySong(lyrics: String)
+}
+
+
+
+
