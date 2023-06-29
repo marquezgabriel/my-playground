2926827244ba2e7f370313b65cee298c43062ddb
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-06-29 17:18:32 -0400

    Implementing Delegates

--- ImplementADelegate.playground/Contents.swift
+++ ImplementADelegate.playground/Contents.swift
@@ -1,19 +1,34 @@
-
-
 protocol Driver {
     var name: String { get }
     func driveToDestination(_ destination: String, with food: String)
 }
+
 class DeliveryDriver: Driver {
     let name: String
+    
     init(name: String) {
         self.name = name
     }
+    
     func driveToDestination(_ destination: String, with food: String) {
         print("\(name) is driving to \(destination) to deliver \(food).")
     }
+    
+    func deliverFood(_ food: String, to destination: String) {
+        if let deliveryDriver = deliveryDriver {
+            driveToDestination(destination, with: food)
+        } else {
+            print("No delivery driver assigned.")
+        }
+    }
 }
 
-let deliveryDriver = DeliveryDriver(name: "Bob")
+class LittleLemon {
+    var deliveryDriver: Driver?
+}
+
+
+let deliveryDriver = DeliveryDriver(name: "Elisa")
 deliveryDriver.driveToDestination("123 Main Street", with: "Pizza")
+
 
