1bc8118d83f2698fc4ee19d8ed44333cf810751c
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-07-05 11:22:38 -0400

    Functional Programmin

---
+++ FunctionalProgramming.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let originalPrices = [2, 5, 6, 12, 5, 20]
+
+func convertPrices(originalPrices: [Int], conversionClosure:(_ price: Int) -> Int) -> [Int] {
+    var convertedPrices = [Int]()
+    for originalPrice in originalPrices {
+        let convertedPrice = conversionClosure(originalPrice)
+        convertedPrices.append(convertedPrice)
+    }
+    return convertedPrices
+}
+
+convertPrices(originalPrices: originalPrices, conversionClosure: {$0 * 2})
