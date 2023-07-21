e7567a760bf0fd855a1c4625a8956cb485829c69
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-07-21 08:30:45 -0400

    Little Lemon Dinner Menu - Task 3 completed

---
+++ Little_Lemon_Dinner_Menu/Little_Lemon_Dinner_Menu/Ingredient.swift
@@ -0,0 +1,16 @@
+//
+//  Ingredient.swift
+//  Little_Lemon_Dinner_Menu
+//
+//  Created by Gabriel Marquez on 2023-07-21.
+//
+
+import Foundation
+
+enum Ingredient: String, CaseIterable {
+    case spinach = "Spinach"
+    case broccoli = "Broccoli"
+    case carrot = "Carrot"
+    case pasta = "Pasta"
+    case tomatoSauce = "Tomato sauce"
+}
